b56e1eb76502e27ca1616e34f7d35800048484b2
Vineet Mali <7038vineet@gmail.com>
2025-01-24 21:48:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
  
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method")); 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs"); 
 app.set("views", path.join(__dirname, "/views")); 
