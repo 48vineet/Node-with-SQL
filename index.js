c6bc1ddc217451e2673be43ccae43cce717ac923
Vineet Mali <7038vineet@gmail.com>
2025-01-24 21:48:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const express = require("express");  
 const app = express();
 const port = 3000;
-
+ 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs"); 
