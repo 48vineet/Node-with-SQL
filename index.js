c11d833c9edc550e54189edcef5172d1e54e2a93
Vineet Mali <7038vineet@gmail.com>
2025-02-21 23:19:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const express = require("express");  
 const app = express();  
-const port = 3000; 
+const port = 3000;  
   
 app.use(methodOverride("_method")); 
 app.use(express.urlencoded({ extended: true }));
